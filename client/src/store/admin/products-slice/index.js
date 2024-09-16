eeaed13614a95b5b75803cf644e8b92f902ef4f8
Rijo Sebastian <rijobdk@gmail.com>
2024-09-16 05:54:11 +0530

    Add product management functionality to Redux store

---
+++ client/src/store/admin/products-slice/index.js
@@ -0,0 +1,89 @@
+import { createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  products: [],
+  isLoading: false,
+};
+
+const backendURL = import.meta.env.VITE_BACKEND_URL;
+
+// Add new product
+export const addNewProduct =  createAsyncThunk('/products/addNewProduct', async (formData)=> {
+    const response = await axios.post(
+      `${backendURL}/api/admin/products/add-product`,
+      formData,{
+        headers: {
+            'Content-Type': 'application/json',
+        }
+      }
+    );
+    return response?.data;
+
+})
+
+// Fetch all products
+export const fetchAllProducts = createAsyncThunk('/products/fetchAllProducts', async ()=> {
+    const response = await axios.get(
+      `${backendURL}/api/admin/products/fetch-all-products`,
+      {
+        headers: {
+            'Content-Type': 'application/json',
+        }
+      }
+    );
+    return response?.data;
+})
+ 
+
+// Edit product
+export const editProduct =  createAsyncThunk('/products/editProduct', async ({formData, id})=> {
+    const response = await axios.put(
+      `${backendURL}/api/admin/products/edit-product/${id}`,
+      formData,{
+        headers: {
+            'Content-Type': 'application/json',
+        }
+      }
+    );
+    return response?.data;
+
+})
+ 
+
+// Delete product
+export const deleteProduct =  createAsyncThunk('/products/deleteProduct', async (id)=> {
+    const response = await axios.delete(
+      `${backendURL}/api/admin/products/delete-product/${id}`,
+      {
+        headers: {
+            'Content-Type': 'application/json',
+        }
+      }
+    );
+    return response?.data;
+
+})
+ 
+
+
+const adminProductsSlice = createSlice({
+    name: 'adminProducts',
+    initialState,
+    reducers : {},
+    extraReducers: (builder)=> {
+        builder.addCase(fetchAllProducts.pending, (state)=>{
+            state.isLoading = true;
+            state.products = [];
+        }).addCase(fetchAllProducts.fulfilled,(state,action)=>{
+            state.isLoading = false;
+            console.log(action.payload);
+            state.products = action.payload;
+        }).addCase(fetchAllProducts.rejected,(state)=>{
+            state.isLoading = false;
+            state.products = [];
+        })
+    }
+})
+
+export  default adminProductsSlice.reducer;
