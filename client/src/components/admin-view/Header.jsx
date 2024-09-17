fc8eaa5d84df21c3f2818e4005b92b2807f7c6a4
Rijo Sebastian <rijobdk@gmail.com>
2024-09-17 22:24:38 +0530

    feat: Add shopping view header with Shadcn components

--- client/src/components/admin-view/Header.jsx
+++ client/src/components/admin-view/Header.jsx
@@ -1,4 +1,4 @@
- import { logout } from "@/store/auth-slice";
+import { logout } from "@/store/auth-slice";
 import { Button } from "../ui/button";
 import { AlignJustify, LogOut } from "lucide-react";
 import { useDispatch } from "react-redux";
@@ -6,10 +6,7 @@ import { useDispatch } from "react-redux";
 export default function AdminHeader({ open, setOpen }) {
   const dispatch = useDispatch();
   function handleLogout() {
-    dispatch(logout()).then((data)=>{
-      console.log(data, "data after logout")
-      if(data?.payload?.success){}
-    })
+    dispatch(logout());
   }
   return (
     <header className="flex items-center justify-center px-4 py-3 bg-background border-b ">
@@ -18,7 +15,10 @@ export default function AdminHeader({ open, setOpen }) {
         <span className="sr-only"> Toggle Menu</span>
       </Button>
       <div className="flex flex-1 justify-end">
-        <Button className="inline-flex gap-2 items-center rounded-md px-4 py-2 text-sm font-medium shadow" onClick={handleLogout}>
+        <Button
+          className="inline-flex gap-2 items-center rounded-md px-4 py-2 text-sm font-medium shadow"
+          onClick={handleLogout}
+        >
           <LogOut /> Logout
         </Button>
       </div>
--- client/src/components/shopping-view/Header.jsx
+++ client/src/components/shopping-view/Header.jsx
@@ -1,7 +1,109 @@
-import React from 'react'
+import { Button } from "../ui/button";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { shoppingViewHeaderMenuItems } from "@/config";
+import { House, LogOut, Menu, ShoppingCart, User } from "lucide-react";
+import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+} from "../ui/dropdown-menu";
+import { DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import { Avatar, AvatarFallback } from "../ui/avatar";
+import { logout } from "@/store/auth-slice";
 
 export default function ShoppingHeader() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { isAuthenticated, user } = useSelector((state) => state.auth);
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+  //  This function renders the nav links
+  const MenuItems = () => {
+    return (
+      <nav className="flex flex-col mb-3 lg:mb-0 lg:items-center gap-6 lg:flex-row">
+        {shoppingViewHeaderMenuItems.map((item) => (
+          <Link key={item.id} to={item.path} className="text-sm font-medium">
+            {item.label}
+          </Link>
+        ))}
+      </nav>
+    );
+  };
+
+  const HeaderRightContent = () => {
+    return (
+      <div className="flex lg:items-center lg:flex-row flex-col gap-4">
+        <Button variant="outline" size="icon">
+          <ShoppingCart className="w-6 h-6" />
+          <span className="sr-only">Cart</span>
+        </Button>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Avatar className="bg-black">
+              <AvatarFallback className="bg-black text-white font-extrabold cursor-pointer">
+                <span>
+                  {user?.userName
+                    ?.split(" ")
+                    .map((item) => item[0].toUpperCase())}
+                </span>
+              </AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent side="right" className="w-56 ">
+            <DropdownMenuLabel>Logged in as {user?.userName}</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={() => navigate("/shop/account")}>
+              <User className="mr-2 h-4 w-4" />
+              Account
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={handleLogout}>
+              <LogOut className="mr-2 h-4 w-4" />
+              Logout
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    );
+  };
+
   return (
-    <div>ShoppingHeader</div>
-  )
+    <header className="sticky top-0 z-40 w-full border-b bg-background">
+      <div className="flex h-16 items-center justify-between px-4 md:px-6">
+        <Link className="flex items-center gap-2" to="/shop/home">
+          <House className="w-6 h-6" />
+          <span className="font-bold">ShopEase</span>
+        </Link>
+        {/* // This is for mobile device hamburger menu */}
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon" className="lg:hidden">
+              <Menu className="w-6 h-6" />
+              <span className="sr-only">Toggle menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left" className="w-full max-w-xs">
+            {/* // nav links for mobile device */}
+            <MenuItems />
+            <HeaderRightContent />
+          </SheetContent>
+        </Sheet>
+
+        {/* // Nav links for desktop device */}
+        <div className="hidden lg:block">
+          <MenuItems />
+        </div>
+
+        <div className="hidden lg:block">
+          <HeaderRightContent />
+        </div>
+      </div>
+    </header>
+  );
 }
