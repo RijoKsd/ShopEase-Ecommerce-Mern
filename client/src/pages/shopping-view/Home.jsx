97b99c6abbfba5946ecc72912875f27cf93fec69
Rijo Sebastian <rijobdk@gmail.com>
2024-09-24 11:40:08 +0530

    Add functionality to navigate and filter by category and brand, implement product section on Home page

--- client/src/pages/shopping-view/Home.jsx
+++ client/src/pages/shopping-view/Home.jsx
@@ -18,26 +18,44 @@ import {
 } from "lucide-react";
 import { Card, CardContent } from "@/components/ui/card";
 import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAllFilteredProducts } from "@/store/shop/products-slice";
+import ShoppingProductTile from "./ProductTile";
+import { useNavigate } from "react-router-dom";
+
+const categories = [
+  { id: "men", label: "Men", icon: User },
+  { id: "women", label: "Women", icon: UserCircle },
+  { id: "kids", label: "Kids", icon: Baby },
+  { id: "accessories", label: "Accessories", icon: Glasses },
+  { id: "footwear", label: "Footwear", icon: Footprints },
+];
+
+const brands = [
+  { id: "nike", label: "Nike", icon: Footprints },
+  { id: "adidas", label: "Adidas", icon: Shirt },
+  { id: "puma", label: "Puma", icon: Cat },
+  { id: "levi", label: "Levi's", icon: Scissors },
+  { id: "zara", label: "Zara", icon: ShoppingBag },
+  { id: "h&m", label: "H&M", icon: Store },
+];
+
 export default function ShoppingHome() {
   const [currentSlide, setCurrentSlide] = useState(0);
-  const slides = [bannerTwo,bannerThree, bannerOne];
+  const slides = [bannerTwo, bannerThree, bannerOne];
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { products } = useSelector((state) => state.shopProducts);
 
-  const categories = [
-    { id: "men", label: "Men", icon: User },
-    { id: "women", label: "Women", icon: UserCircle },
-    { id: "kids", label: "Kids", icon: Baby },
-    { id: "accessories", label: "Accessories", icon: Glasses },
-    { id: "footwear", label: "Footwear", icon: Footprints },
-  ];
+  function handleNavigateToProductListing(currentItem, section) {
+    sessionStorage.removeItem("filters");
+    const currentFilter = {
+      [section]: [currentItem.id],
+    };
+    sessionStorage.setItem('filters', JSON.stringify(currentFilter))
 
-  const brands = [
-    { id: "nike", label: "Nike", icon: Footprints },
-    { id: "adidas", label: "Adidas", icon: Shirt },
-    { id: "puma", label: "Puma", icon: Cat },
-    { id: "levi", label: "Levi's", icon: Scissors },
-    { id: "zara", label: "Zara", icon: ShoppingBag },
-    { id: "h&m", label: "H&M", icon: Store },
-  ];
+    navigate("/shop/listing");
+  }
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -47,6 +65,14 @@ export default function ShoppingHome() {
     return () => clearInterval(interval);
   }, [slides]);
 
+  useEffect(() => {
+    dispatch(
+      fetchAllFilteredProducts({
+        filterParams: {},
+        sortParams: "price-low-to-high",
+      })
+    );
+  }, [dispatch]);
   return (
     <div className="flex flex-col min-h-screen">
       {/* Home banners */}
@@ -89,13 +115,16 @@ export default function ShoppingHome() {
       <section className="py-12 bg-gray-50">
         <div className="container mx-auto px-4">
           <h2 className="text-3xl font-bold text-center mb-8">
-            Shop by category
+            Shop by Category
           </h2>
           <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
             {categories.map((category) => (
               <Card
-                className="cursor-pointer hover:shadow-lg transition-shadow"
                 key={category.id}
+                className="cursor-pointer hover:shadow-lg transition-shadow"
+                onClick={() =>
+                  handleNavigateToProductListing(category, "category")
+                }
               >
                 <CardContent className="flex flex-col items-center  justify-center p-6">
                   <category.icon className="w-12 h-12 mb-4 text-primary" />
@@ -104,14 +133,22 @@ export default function ShoppingHome() {
               </Card>
             ))}
           </div>
+        </div>
+      </section>
 
-          <h2 className="text-3xl font-bold text-center mb-8">Shop by brand</h2>
+      {/* Brands */}
+      <section className="py-12 bg-gray-50">
+        <div className="container mx-auto px-4">
+          <h2 className="text-3xl font-bold text-center mb-8">Shop by Brand</h2>
 
-          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4">
             {brands.map((brand) => (
               <Card
+              key={brand.id}
                 className="cursor-pointer hover:shadow-lg transition-shadow"
-                key={brand.id}
+                onClick={() =>
+                  handleNavigateToProductListing(brand, "brand")
+                }
               >
                 <CardContent className="flex flex-col items-center  justify-center p-6">
                   <brand.icon className="w-12 h-12 mb-4 text-primary" />
@@ -119,6 +156,27 @@ export default function ShoppingHome() {
                 </CardContent>
               </Card>
             ))}
+          </div>
+        </div>
+      </section>
+
+      {/* Featured Products */}
+
+      <section className="py-12">
+        <div className="container mx-auto px-4">
+          <h2 className="text-3xl font-bold text-center mb-8">
+            Feature Products
+          </h2>
+          <div className="grid gri-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            {products && products.length > 0 ? (
+              products
+                .slice(0, 4)
+                .map((product) => (
+                  <ShoppingProductTile key={product?._id} product={product} />
+                ))
+            ) : (
+              <h1>No products found</h1>
+            )}
           </div>
         </div>
       </section>
